dfc5593097f742d54737e97b88f6d6a07527ee03
Allenzzp <131452abc@gmail.com>
2021-12-08 08:03:12 +0000

    first commit to this branch

--- express_server.js
+++ express_server.js
@@ -36,28 +36,40 @@ function makeFullURL(url) {
   }
   return url;
 }
+function emailExist(email, obj) {
+  for (let key in obj) {
+    if (obj[key]["email"] === email) {
+      return true;
+    }
+  }
+  return false;
+}
 
-//handle GET
+//databse
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+};
+
+//handle GET
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls", (req, res) => {
   const templateVars = { 
-  urls: urlDatabase,
-  username: req.cookies["username"], 
+    urls: urlDatabase,
+    user: users[req.cookies["user_id"]], 
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"], 
+    user: users[req.cookies["user_id"]], 
   };
   res.render("urls_new", templateVars);
 });
@@ -66,7 +78,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
   };
   res.render("urls_show", templateVars);
 });
@@ -75,6 +87,13 @@ app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
   longURL = makeFullURL(longURL);
   res.redirect(longURL);
+});
+
+app.get("/register", (req, res) => {
+  const templateVars = {
+    user: users[req.cookies["user_id"]],
+  }
+  res.render("register", templateVars);
 });
 
 //handle POST
@@ -103,18 +122,47 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  if (!req.cookies.username) {
-    res.cookie("username", req.body.username, {
-      expires: new Date(Date.now() + 24 * 3600000) //cookies expire after 24 hours
-    });
+  if (urls[req.cookies["user_id"]].email === req.body.email) {
+    res.redirect("/urls");
   }
-  res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
   res.clearCookie("username", req.cookies["username"]);
   res.redirect("/urls");
-})
+});
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const templateVars = {
+    desc: "",
+  }; 
+  
+  if (email === "") {
+    templateVars.desc = "noemail";
+  } else if (password === "") {
+    templateVars.desc = "nopassword";
+  } else if (emailExist(email, users)) {
+    templateVars.desc = "emailexist";
+  }
+
+  if (templateVars.desc !== "") {
+    res.status(400);
+    res.render("errors", templateVars);
+  } else {
+    const id = geneteRandomString();
+    users[id] = {
+      id,
+      email,
+      password,
+    };
+    res.cookie("user_id", id);
+    res.redirect("/urls");
+  }
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
